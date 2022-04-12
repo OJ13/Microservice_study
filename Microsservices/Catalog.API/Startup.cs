9ea04bae61c42b971ff66f64dfe64008573bec2e
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-04-11 22:39:58 -0300

    add repositories e connection string mongo

--- Microsservices/Catalog.API/Startup.cs
+++ Microsservices/Catalog.API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.API.Data;
+using Catalog.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +26,8 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
         }
 
